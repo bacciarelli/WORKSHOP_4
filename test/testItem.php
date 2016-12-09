ffe253c35145450c6f2414cb3997912ea35d5947
Imsaho <jo.bednarczyk@gmail.com>
2016-12-09 15:38:36 +0100

    added tests for Item class

--- test/testItem.php
+++ test/testItem.php
@@ -6,7 +6,12 @@ class testItem extends PHPUnit_Framework_TestCase {
     
     protected function setUp() {
         parent::setUp();
-        $this->newObject = new Item();
+        $item = $this->newObject = new Item();
+        $this->newObject->setItemName("czekolada mleczna");
+        $this->newObject->setDescription("opis czekolady");
+        $this->newObject->setPrice(3.99);
+        $this->newObject->setStockQuantity(100);
+        $this->newObject->setCategoryId(1);
     }
     
     protected function tearDown() {
@@ -15,6 +20,55 @@ class testItem extends PHPUnit_Framework_TestCase {
     }
     
     public function testInstanceOfObject() {
-        
+        $this->assertTrue($this->newObject instanceof Item);
+    }
+    
+    public function testGetId() {
+        $this->assertEquals(-1, $this->newObject->getId());
+    }
+    
+    public function testGetItemName() {
+        $this->assertSame("czekolada mleczna", $this->newObject->getItemName());
+    }
+    
+    public function testGetPrice() {
+        $this->assertSame(3.99, $this->newObject->getPrice());
+    }
+    
+    public function testGetDescription() {
+        $this->assertSame("opis czekolady", $this->newObject->getDescription());
+    }
+    
+    public function testGetStockQuantity() {
+        $this->assertSame(100, $this->newObject->getStockQuantity());
+    }
+    
+    public function testGetCategoryId() {
+        $this->assertSame(1, $this->newObject->getCategoryId());
+    }
+    
+    public function testSetItemName() {
+        $this->newObject->setItemName("czekolada ciemna");
+        $this->assertEquals("czekolada ciemna", $this->newObject->getItemName());
+    }
+    
+    public function testSetPrice() {
+        $this->newObject->setPrice(1.99);
+        $this->assertEquals(1.99, $this->newObject->getPrice());
+    }
+    
+    public function testSetDescription() {
+        $this->newObject->setDescription("nowy opis");
+        $this->assertEquals("nowy opis", $this->newObject->getDescription());
+    }
+    
+    public function testSetStockQuantity() {
+        $this->newObject->setStockQuantity(1);
+        $this->assertEquals(1, $this->newObject->getStockQuantity());
+    }
+    
+    public function testSetCategoryId() {
+        $this->newObject->setCategoryId(11);
+        $this->assertEquals(11, $this->newObject->getCategoryId());
     }
 }
